d9363a66d35d7c64286eaf80254aa996e4c18582
John Doe <Johndoe@example.com>
2016-11-02 14:41:08 -0700

    first commit

---
+++ models/genre.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+//Genre Schema
+
+var genreSchema  = mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema)
+
+//Get Genres
+module.exports.getGenres = function(callback, limit){
+	Genre.find(callback).limit(limit);
+}
+
+
+//Add Genres
+module.exports.addGenres = function(genre, callback){
+	Genre.create(genre, callback);
+}
+
+//Update Genres
+module.exports.updateGenres = function(id, genre, option, callback){
+	var query = {_id: id};
+	var update = {
+		name: genre.name
+	}
+	Genre.findOneAndUpdate(query, update, option, callback);
+}
+
+//Delete Genres
+module.exports.removeGenre = function(id, callback){
+	var query = {_id: id};
+	Genre.remove(query, callback);
+}
